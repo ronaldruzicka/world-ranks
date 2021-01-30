79769c0b665788a273f23a9a44f1232a51861282
Ronald Ruzicka <ruzicka.ronald@gmail.com>
2021-01-30 17:04:50 +0100

    Set max width for container

--- src/components/Container.ts
+++ src/components/Container.ts
@@ -1,6 +1,9 @@
-import { padding } from 'polished'
+import { padding, margin } from 'polished'
 import styled from 'styled-components'
 
 export const Container = styled.div`
   ${({ theme }) => padding(null, theme.spacer * 3)};
+  ${margin(null, 'auto')};
+  display: block;
+  max-width: 1200px;
 `
